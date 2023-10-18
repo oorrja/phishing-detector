f293ffcb39bda3f660b43f442ba35c482e5e4b7f
oorrja <63793935+oorrja@users.noreply.github.com>
2023-10-18 14:21:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for, request
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Load the model and CountVectorizer from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('tranform.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
